ff2b4425e4ca7b7df19c4b35e8ba02bff5355aa9
psirenny <djtorres0@gmail.com>
2018-10-22 10:43:54 -0400

    chore: cleanup

--- src/swift/AppleAVAudioSessionGetRecordPermission.swift
+++ src/swift/AppleAVAudioSessionGetRecordPermission.swift
@@ -2,7 +2,7 @@ import AVFoundation
 
 @objc(AppleAVAudioSessionGetRecordPermission)
 class AppleAVAudioSessionGetRecordPermission: CDVPlugin {
-  @objc(exec:) func index(_ command: CDVInvokedUrlCommand) {
+  @objc(exec:) func exec(_ command: CDVInvokedUrlCommand) {
     let permission = AVAudioSession.sharedInstance().recordPermission()
     let result = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: permission.rawValue)
     self.commandDelegate!.send(result, callbackId: command.callbackId)
